1eda31e5bdabccfcbd7893a32dc277657ff69362
Zhihao Qian <zqian3@ualberta.ca>
2024-10-24 17:42:25 -0600

    Implement addCity method and fix syntax errors

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -6,14 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import java.util.ArrayList;
 
 public class CustomList extends ArrayAdapter<City> {
-
     private ArrayList<City> cities;
     private Context context;
 
@@ -26,15 +23,12 @@ public class CustomList extends ArrayAdapter<City> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
         View view = convertView;
-
-        if(view == null){
-            view = LayoutInflater.from(context).inflate(R.layout.content, parent,false);
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.content, parent, false);
         }
 
         City city = cities.get(position);
-
         TextView cityName = view.findViewById(R.id.city_text);
         TextView provinceName = view.findViewById(R.id.province_text);
 
@@ -42,15 +36,22 @@ public class CustomList extends ArrayAdapter<City> {
         provinceName.setText(city.getProvinceName());
 
         return view;
-
     }
 
-    public int getCount(){
+    /**
+     * This gets the size of the list.
+     * @return the number of cities in the list.
+     */
+    public int getCount() {
         return cities.size();
     }
 
-    public void addCity(City city){
-
+    /**
+     * This adds a city object to the list.
+     * @param city the city to add.
+     */
+    public void addCity(City city) {
+        cities.add(city);
     }
-
 }
+
--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,16 +1,30 @@
 package com.example.lab8;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private CustomList list;
 
+    /**
+     * Create a mock list for testing.
+     * @return a mock CustomList instance.
+     */
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
 
+    /**
+     * Test if adding a city increases the list size by one.
+     */
+    @Test
+    public void addCityTest() {
+        list = MockCityList();
+        int initialSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(initialSize + 1, list.getCount());
+    }
 }
+
